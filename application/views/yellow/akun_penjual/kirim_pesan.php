29699c414515cf0df96ec24fca5fc25f6ca7e6d5
zuhdijung <zuhdi.robbani@gmail.com>
2016-11-17 14:36:15 +0700

    desain baru

---
+++ application/views/yellow/akun_penjual/kirim_pesan.php
@@ -0,0 +1,68 @@
+<div class="container" style="margin-bottom:70px">
+  <div class="modal-title text-center" style="margin-bottom:25px">
+        Kirim Pesan
+  </div>
+  <div class="row text-center">
+    <div class="col-md-4">
+      <ul class="member-sidebar text-left" id="member-sidebar" style="list-style:none">
+        <li>
+          <a class="active" href="#"><i class="fa fa-user" aria-hidden="true"></i>Profil</a>
+        </li>
+        <li>
+          <a href="profile.html"><i class="fa fa-envelope-open" aria-hidden="true"></i> Pesan</a>
+        </li>
+        <li>
+          <a href="profile.html"><i class="fa fa-history" aria-hidden="true"></i> Riwayat Order</a>
+        </li>
+        <li>
+          <a href="ChangePassword.html"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Desain</a>
+        </li>
+        <li>
+          <a href="certified.html"><i class="fa fa-key" aria-hidden="true"></i>Change Password</a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-8">
+      <form class="fr">
+      <div class="form-group">
+        <div class="row">
+          <div class="col-md-3 col-sm-3 col-xs-3 text-right">
+            <label class="control-label">Penerima Pesan</label>
+          </div>
+          <div class="col-md-6 col-sm-6 col-xs-9">
+            <input type="text" class="form-control col-md-7 col-xs-12">
+          </div>
+          <div class="col-md-3 col-sm-3 hidden-xs"></div>
+        </div>
+      </div>      
+      <div class="form-group">
+        <div class="row">
+          <div class="col-md-3 col-sm-3 col-xs-3 text-right">
+            <label class="control-label">Isi Pesan</label>
+          </div>
+          <div class="col-md-6 col-sm-6 col-xs-9">
+            <textarea class="form-control col-md-7 col-xs-12" rows="5" id="comment"></textarea>
+          </div>
+          <div class="col-md-3 col-sm-3 hidden-xs"></div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="row">
+          <div class="col-md-3 col-sm-3 col-xs-3 text-right">
+            <label class="control-label">Gambar Pesan</label>
+          </div>
+          <div class="col-md-6 col-sm-6 col-xs-9">
+             <input type="file" name="pic" class="form-control col-md-7 col-xs-12">
+            <a class="btn btn-md form-control fur-btn-primary" style="margin-top:10px" href="#" role="button" type="submit">
+              Submit
+            </a>
+          </div>
+          <div class="col-md-3 col-sm-3 hidden-xs"></div>
+        </div>
+      </div>
+      </form>
+    </div>
+       
+    </div>
+  </div>
+</div>
---
+++ application/views/yellow/akun_penjual/riwayat_produk.php
@@ -0,0 +1,118 @@
+<div class="container" style="margin-bottom:70px">
+  <div class="modal-title text-center" style="margin-bottom:25px">
+        Riwayat Produk
+  </div>
+  <div class="row text-center">
+    <div class="col-md-3">
+      <ul class="member-sidebar text-left" id="member-sidebar" style="list-style:none">
+        <li>
+          <a href="#"><i class="fa fa-user" aria-hidden="true"></i>Profil</a>
+        </li>
+        <li>
+          <a href="profile.html"><i class="fa fa-envelope-open" aria-hidden="true"></i> Pesan</a>
+        </li>
+        <li>
+          <a href="profile.html"><i class="fa fa-history" aria-hidden="true"></i> Riwayat Order</a>
+        </li>
+        <li>
+          <a href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Desain</a>
+        </li>
+        <li>
+          <a class="active"  href="ganti_password.html"><i class="fa fa-key" aria-hidden="true"></i>Pesan</a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-8 form-group form-impian table-responsive">
+      <div class="row">
+      </div>
+      <table class="table table-striped profile" style="border:1px solid #ddd">
+        <thead> 
+          <tr>
+            <td>No</td>
+            <td style="padding:0px 50px 0px 50px">Kategori</td>
+            <td>Gambar</td>
+            <td>Nama Produk</td>
+            <td>Harga</td>
+            <td style="padding:0px 50px 0px 50px">Deskripsi</td>
+            <td>Aksi</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Lorem Ipsum Dolor Sil Amet</td>
+            <td>
+              <img class="img-responsive" src="asset/images/sofa1.jpg">
+            </td>
+            <td>Sofa Ketje</td>
+            <td>Rp 1,300,000</td>
+            <td>Lorem Ipsum Dolor Sil Amet Lorem Ipsum Dolor Sil Amet Lorem Ipsum Dolor Sil Amet</td>
+            <td>
+              <button class="btn btn-default dropdown-toggle">
+                <i class="fa fa-pencil"></i>
+              </button>
+              <button class="btn btn-default dropdown-toggle">
+                <i class="fa fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Lorem Ipsum Dolor Sil Amet</td>
+            <td>
+              <img class="img-responsive" src="asset/images/sofa1.jpg">
+            </td>
+            <td>Sofa Ketje</td>
+            <td>Rp 1,300,000</td>
+            <td>Lorem Ipsum Dolor Sil Amet Lorem Ipsum Dolor Sil Amet Lorem Ipsum Dolor Sil Amet</td>
+            <td>
+              <button class="btn btn-default dropdown-toggle">
+                <i class="fa fa-pencil"></i>
+              </button>
+              <button class="btn btn-default dropdown-toggle">
+                <i class="fa fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Lorem Ipsum Dolor Sil Amet</td>
+            <td>
+              <img class="img-responsive" src="asset/images/sofa1.jpg">
+            </td>
+            <td>Sofa Ketje</td>
+            <td>Rp 1,300,000</td>
+            <td>Lorem Ipsum Dolor Sil Amet Lorem Ipsum Dolor Sil Amet Lorem Ipsum Dolor Sil Amet</td>
+            <td>
+              <button class="btn btn-default dropdown-toggle">
+                <i class="fa fa-pencil"></i>
+              </button>
+              <button class="btn btn-default dropdown-toggle">
+                <i class="fa fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Lorem Ipsum Dolor Sil Amet</td>
+            <td>
+              <img class="img-responsive" src="asset/images/sofa1.jpg">
+            </td>
+            <td>Sofa Ketje</td>
+            <td>Rp 1,300,000</td>
+            <td>Lorem Ipsum Dolor Sil Amet Lorem Ipsum Dolor Sil Amet Lorem Ipsum Dolor Sil Amet</td>
+            <td>
+              <button class="btn btn-default dropdown-toggle">
+                <i class="fa fa-pencil"></i>
+              </button>
+              <button class="btn btn-default dropdown-toggle">
+                <i class="fa fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+  </table>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
+</div>
